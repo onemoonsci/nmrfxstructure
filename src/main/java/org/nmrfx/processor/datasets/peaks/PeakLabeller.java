f7c020d0e2a5c48a420c415e72c807cb8589b029
Bruce Johnson <bruce.johnson@asrc.cuny.edu>
2020-01-30 09:31:46 -0500

    Add new PeakLabeller (with method to add single res char to peak label)

---
+++ src/main/java/org/nmrfx/processor/datasets/peaks/PeakLabeller.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.nmrfx.processor.datasets.peaks;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.nmrfx.structure.chemistry.Atom;
+import org.nmrfx.structure.chemistry.Molecule;
+import org.nmrfx.structure.chemistry.Residue;
+
+/**
+ *
+ * @author brucejohnson
+ */
+public class PeakLabeller {
+
+    static Pattern rPat = Pattern.compile("^(.+:)?(([a-zA-Z]+)([0-9\\-]+))\\.(['a-zA-Z0-9u]+)$");
+    static Pattern rPat2 = Pattern.compile("^(.+:)?([0-9\\-]+)\\.(['a-zA-Z0-9u]+)$");
+
+    public static void labelWithSingleResidueChar(PeakList peakList) {
+        peakList.peaks().stream().forEach(pk -> {
+            for (PeakDim peakDim : pk.getPeakDims()) {
+                String label = peakDim.getLabel();
+                Matcher matcher1 = rPat.matcher(label);
+                if (!matcher1.matches()) {
+                    Matcher matcher2 = rPat2.matcher(label);
+                    if (matcher2.matches()) {
+                        String chain = matcher2.group(1);
+                        String resNum = matcher2.group(2);
+                        String aName = matcher2.group(3);
+                        String atomSpec = chain + resNum + "." + aName;
+                        Atom atom = Molecule.getAtomByName(atomSpec);
+                        if (atom != null) {
+                            if (atom.getEntity() instanceof Residue) {
+                                char oneChar = ((Residue) atom.getEntity()).getOneLetter();
+                                String newAtomSpec = chain + oneChar + resNum + "." + aName;
+                                peakDim.setLabel(newAtomSpec);
+                            }
+                        }
+                    }
+                }
+            }
+        });
+    }
+}
